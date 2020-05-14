98262439e9e80ef483254021cbf1bdf19886c20f
Isaac Abraham <isaac.abraham@gmail.com>
2020-05-14 14:37:10 +0200

    Updates

--- OldLinq/OldImpl.cs
+++ OldLinq/OldImpl.cs
@@ -1,10 +1,292 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace System.Linq
 {
     public static class OldEnumerable
     {
+        static Func<TSource, bool> CombinePredicates<TSource>(Func<TSource, bool> predicate1, Func<TSource, bool> predicate2) {
+            return x => predicate1(x) && predicate2(x);
+        }
+        static Func<TSource, TResult> CombineSelectors<TSource, TMiddle, TResult>(Func<TSource, TMiddle> selector1, Func<TMiddle, TResult> selector2) {
+            return x => selector2(selector1(x));
+        }
+        class WhereSelectEnumerableIterator<TSource, TResult> : Iterator<TResult>
+        {
+            IEnumerable<TSource> source;
+            Func<TSource, bool> predicate;
+            Func<TSource, TResult> selector;
+            IEnumerator<TSource> enumerator;
+
+            public WhereSelectEnumerableIterator(IEnumerable<TSource> source, Func<TSource, bool> predicate, Func<TSource, TResult> selector) {
+                this.source = source;
+                this.predicate = predicate;
+                this.selector = selector;
+            }
+
+            public override Iterator<TResult> Clone() {
+                return new WhereSelectEnumerableIterator<TSource, TResult>(source, predicate, selector);
+            }
+
+            public override void Dispose() {
+                if (enumerator is IDisposable) ((IDisposable)enumerator).Dispose();
+                enumerator = null;
+                base.Dispose();
+            }
+
+            public override bool MoveNext() {
+                switch (state) {
+                    case 1:
+                        enumerator = source.GetEnumerator();
+                        state = 2;
+                        goto case 2;
+                    case 2:
+                        while (enumerator.MoveNext()) {
+                            TSource item = enumerator.Current;
+                            if (predicate == null || predicate(item)) {
+                                current = selector(item);
+                                return true;
+                            }
+                        }
+                        Dispose();
+                        break;
+                }
+                return false;
+            }
+
+            public override IEnumerable<TResult2> Select<TResult2>(Func<TResult, TResult2> selector) {
+                return new WhereSelectEnumerableIterator<TSource, TResult2>(source, predicate, CombineSelectors(this.selector, selector));
+            }
+
+            public override IEnumerable<TResult> Where(Func<TResult, bool> predicate) {
+                return new WhereEnumerableIterator<TResult>(this, predicate);
+            }
+        }
+        abstract class Iterator<TSource> : IEnumerable<TSource>, IEnumerator<TSource>
+        {
+            int threadId;
+            internal int state;
+            internal TSource current;
+
+            public Iterator() {
+                threadId = Thread.CurrentThread.ManagedThreadId;
+            }
+
+            public TSource Current {
+                get { return current; }
+            }
+
+            public abstract Iterator<TSource> Clone();
+
+            public virtual void Dispose() {
+                current = default(TSource);
+                state = -1;
+            }
+
+            public IEnumerator<TSource> GetEnumerator() {
+                if (threadId == Thread.CurrentThread.ManagedThreadId && state == 0) {
+                    state = 1;
+                    return this;
+                }
+                Iterator<TSource> duplicate = Clone();
+                duplicate.state = 1;
+                return duplicate;
+            }
+
+            public abstract bool MoveNext();
+
+            public abstract IEnumerable<TResult> Select<TResult>(Func<TSource, TResult> selector);
+
+            public abstract IEnumerable<TSource> Where(Func<TSource, bool> predicate);
+
+            object IEnumerator.Current {
+                get { return Current; }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() {
+                return GetEnumerator();
+            }
+
+            void IEnumerator.Reset() {
+                throw new NotImplementedException();
+            }
+        }
+        class WhereEnumerableIterator<TSource> : Iterator<TSource>
+        {
+            IEnumerable<TSource> source;
+            Func<TSource, bool> predicate;
+            IEnumerator<TSource> enumerator;
+
+            public WhereEnumerableIterator(IEnumerable<TSource> source, Func<TSource, bool> predicate) {
+                this.source = source;
+                this.predicate = predicate;
+            }
+
+            public override Iterator<TSource> Clone() {
+                return new WhereEnumerableIterator<TSource>(source, predicate);
+            }
+
+            public override void Dispose() {
+                if (enumerator is IDisposable) ((IDisposable)enumerator).Dispose();
+                enumerator = null;
+                base.Dispose();
+            }
+
+            public override bool MoveNext() {
+                switch (state) {
+                    case 1:
+                        enumerator = source.GetEnumerator();
+                        state = 2;
+                        goto case 2;
+                    case 2:
+                        while (enumerator.MoveNext()) {
+                            TSource item = enumerator.Current;
+                            if (predicate(item)) {
+                                current = item;
+                                return true;
+                            }
+                        }
+                        Dispose();
+                        break;
+                }
+                return false;
+            }
+
+            public override IEnumerable<TResult> Select<TResult>(Func<TSource, TResult> selector) {
+                return new WhereSelectEnumerableIterator<TSource, TResult>(source, predicate, selector);
+            }
+
+            public override IEnumerable<TSource> Where(Func<TSource, bool> predicate) {
+                return new WhereEnumerableIterator<TSource>(source, CombinePredicates(this.predicate, predicate));
+            }
+        }
+        class WhereArrayIterator<TSource> : Iterator<TSource>
+        {
+            TSource[] source;
+            Func<TSource, bool> predicate;
+            int index;
+
+            public WhereArrayIterator(TSource[] source, Func<TSource, bool> predicate) {
+                this.source = source;
+                this.predicate = predicate;
+            }
+
+            public override Iterator<TSource> Clone() {
+                return new WhereArrayIterator<TSource>(source, predicate);
+            }
+
+            public override bool MoveNext() {
+                if (state == 1) {
+                    while (index < source.Length) {
+                        TSource item = source[index];
+                        index++;
+                        if (predicate(item)) {
+                            current = item;
+                            return true;
+                        }
+                    }
+                    Dispose();
+                }
+                return false;
+            }
+
+            public override IEnumerable<TResult> Select<TResult>(Func<TSource, TResult> selector) {
+                return new WhereSelectArrayIterator<TSource, TResult>(source, predicate, selector);
+            }
+
+            public override IEnumerable<TSource> Where(Func<TSource, bool> predicate) {
+                return new WhereArrayIterator<TSource>(source, CombinePredicates(this.predicate, predicate));
+            }
+        }
+
+        class WhereSelectArrayIterator<TSource, TResult> : Iterator<TResult>
+        {
+            TSource[] source;
+            Func<TSource, bool> predicate;
+            Func<TSource, TResult> selector;
+            int index;
+
+            public WhereSelectArrayIterator(TSource[] source, Func<TSource, bool> predicate, Func<TSource, TResult> selector) {
+                this.source = source;
+                this.predicate = predicate;
+                this.selector = selector;
+            }
+
+            public override Iterator<TResult> Clone() {
+                return new WhereSelectArrayIterator<TSource, TResult>(source, predicate, selector);
+            }
+
+            public override bool MoveNext() {
+                if (state == 1) {
+                    while (index < source.Length) {
+                        TSource item = source[index];
+                        index++;
+                        if (predicate == null || predicate(item)) {
+                            current = selector(item);
+                            return true;
+                        }
+                    }
+                    Dispose();
+                }
+                return false;
+            }
+
+            public override IEnumerable<TResult2> Select<TResult2>(Func<TResult, TResult2> selector) {
+                return new WhereSelectArrayIterator<TSource, TResult2>(source, predicate, CombineSelectors(this.selector, selector));
+            }
+
+            public override IEnumerable<TResult> Where(Func<TResult, bool> predicate) {
+                return new WhereEnumerableIterator<TResult>(this, predicate);
+            }
+        }
+        class WhereSelectListIterator<TSource, TResult> : Iterator<TResult>
+        {
+            List<TSource> source;
+            Func<TSource, bool> predicate;
+            Func<TSource, TResult> selector;
+            List<TSource>.Enumerator enumerator;
+
+            public WhereSelectListIterator(List<TSource> source, Func<TSource, bool> predicate, Func<TSource, TResult> selector) {
+                this.source = source;
+                this.predicate = predicate;
+                this.selector = selector;
+            }
+
+            public override Iterator<TResult> Clone() {
+                return new WhereSelectListIterator<TSource, TResult>(source, predicate, selector);
+            }
+
+            public override bool MoveNext() {
+                switch (state) {
+                    case 1:
+                        enumerator = source.GetEnumerator();
+                        state = 2;
+                        goto case 2;
+                    case 2:
+                        while (enumerator.MoveNext()) {
+                            TSource item = enumerator.Current;
+                            if (predicate == null || predicate(item)) {
+                                current = selector(item);
+                                return true;
+                            }
+                        }
+                        Dispose();
+                        break;
+                }
+                return false;
+            }
+
+            public override IEnumerable<TResult2> Select<TResult2>(Func<TResult, TResult2> selector) {
+                return new WhereSelectListIterator<TSource, TResult2>(source, predicate, CombineSelectors(this.selector, selector));
+            }
+
+            public override IEnumerable<TResult> Where(Func<TResult, bool> predicate) {
+                return new WhereEnumerableIterator<TResult>(this, predicate);
+            }
+        }
+
         public static TSource LastOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) {
             if (source == null) throw new ArgumentNullException("source");
             if (predicate == null) throw new ArgumentNullException("predicate");
@@ -16,7 +298,6 @@ namespace System.Linq
             }
             return result;
         }
-
         public static TSource FirstOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) {
             if (source == null) throw new Exception("source");
             if (predicate == null) throw new Exception("predicate");
@@ -25,7 +306,6 @@ namespace System.Linq
             }
             return default(TSource);
         }
-
         public static TSource SingleOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) {
             if (source == null) throw new Exception("source");
             if (predicate == null) throw new Exception("predicate");
@@ -42,6 +322,15 @@ namespace System.Linq
                 case 1: return result;
             }
             throw new Exception();
+        }
+
+        public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector) {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+            if (source is Iterator<TSource>) return ((Iterator<TSource>)source).Select(selector);
+            if (source is TSource[]) return new WhereSelectArrayIterator<TSource, TResult>((TSource[])source, null, selector);
+            if (source is List<TSource>) return new WhereSelectListIterator<TSource, TResult>((List<TSource>)source, null, selector);
+            return new WhereSelectEnumerableIterator<TSource, TResult>(source, null, selector);
         }
     }
 }
--- Tests/Program.fs
+++ Tests/Program.fs
@@ -1,26 +1,7 @@
 ﻿open Expecto
-open System
 open System.Linq
 open System.Collections.Generic
-
 open FsCheck
-open System
-open System.Collections.Generic
-open System.Linq
-
-[<AutoOpen>]
-module Generation =
-    type Identity = Func<int, int>
-    type Predicate = Func<int, bool>
-    let genIdentity = Arb.Default.Fun<int, int>() |> Arb.toGen
-    let genPredicate = Arb.Default.Fun<int, bool>() |> Arb.toGen
-
-type CollectionMapper = IEnumerable<int> -> IEnumerable<int>
-type Aggregation = IEnumerable<int> -> int
-
-type ComparisonMethod =
-    | CollectionComparison of old:CollectionMapper * current:CollectionMapper
-    | AggregationComparison of name:string * old:Aggregation * current:Aggregation
 
 module Functions =
     let trackCalls predicate func data =
@@ -32,72 +13,48 @@ module Functions =
         func(data, predicate) |> ignore
         count
 
-    let makeAggregation name current old predicate =
-        AggregationComparison(
-            name,
-            (fun data -> trackCalls predicate current data),
-            (fun data -> trackCalls predicate old data))
-
-    let firstOrDefault = makeAggregation "FirstOrDefault" Enumerable.FirstOrDefault OldEnumerable.FirstOrDefault
-    let lastOrDefault = makeAggregation "LastOrDefault" Enumerable.LastOrDefault OldEnumerable.LastOrDefault
-    let singleOrDefault = makeAggregation "SingleOrDefault" Enumerable.SingleOrDefault OldEnumerable.SingleOrDefault
-
-let generateComparison =
-    let aggregator =
-        [ Functions.firstOrDefault
-          Functions.lastOrDefault
-          Functions.singleOrDefault ]
-        |> Gen.elements
-
-    gen {
-        let! predicate = genPredicate
-        let! aggregator = aggregator
-        return aggregator predicate
-    }
-
-type FunctionsGen =
-    static member Aggregator() =
-        { new Arbitrary<ComparisonMethod>() with
-            override _.Generator = generateComparison
-        }
-
 let safely thunk x =
     try Ok(thunk x)
     with ex -> Error ex
 
-type CollectionMapper<'TSource> = IEnumerable<'TSource> -> IEnumerable<'TSource>
-type Aggregation<'TSource> = IEnumerable<'TSource> -> 'TSource
-
-type ComparisonMethod<'TSource> =
-    | CollectionComparison of old:CollectionMapper<'TSource> * current:CollectionMapper<'TSource>
-    | AggregationComparison of old:Aggregation<'TSource> * current:Aggregation<'TSource>
-
 [<Tests>]
 let myTests =
-    let quickCompare netCoreFunc netFrameworkFunc prepare (inputData:int array) predicate =
-        let newResult = inputData |> prepare |> safely (Functions.trackCalls predicate netCoreFunc)
-        let oldResult = inputData |> prepare |> safely (Functions.trackCalls predicate netFrameworkFunc)
+    /// Compares two arbitrary functions that take in two arguments: an array of data, and some
+    /// higher-order function e.g. a predicate or mapper etc.. It records the number of times that
+    /// the higher-order function was called and returns the answer.
+    let quickCompare netCoreFunc netFrameworkFunc prepare (inputData:_ array) higherOrderFunction =
+        let newResult = inputData |> prepare |> safely (Functions.trackCalls higherOrderFunction netCoreFunc)
+        let oldResult = inputData |> prepare |> safely (Functions.trackCalls higherOrderFunction netFrameworkFunc)
         match newResult, oldResult with
-        | Ok a, Ok b -> a = b
-        | Error _, Error _ -> true
-        | _ -> false
+        | Error _, Error _ ->
+            true
+        | _ ->
+            Expect.equal newResult oldResult "Both results should be the same!"
+            true
 
-    testList "LINQ tests" [
-        testProperty "New FirstOrDefault is deterministic" <| quickCompare Enumerable.FirstOrDefault Enumerable.FirstOrDefault id
-        testProperty "New LastOrDefault is deterministic" <| quickCompare Enumerable.LastOrDefault Enumerable.LastOrDefault id
-        testProperty "New SingleOrDefault is deterministic" <| quickCompare Enumerable.SingleOrDefault Enumerable.SingleOrDefault id
-        testProperty "Original FirstOrDefault is deterministic" <| quickCompare OldEnumerable.FirstOrDefault OldEnumerable.FirstOrDefault id
-        testProperty "Original LastOrDefault is deterministic" <| quickCompare OldEnumerable.LastOrDefault OldEnumerable.LastOrDefault id
-        testProperty "Original SingleOrDefault is deterministic" <| quickCompare OldEnumerable.SingleOrDefault OldEnumerable.SingleOrDefault id
+    testList "All" [
+        testList "Deterministic tests" [
+            testProperty "New FirstOrDefault is deterministic" <| quickCompare Enumerable.FirstOrDefault Enumerable.FirstOrDefault id
+            testProperty "New LastOrDefault is deterministic" <| quickCompare Enumerable.LastOrDefault Enumerable.LastOrDefault id
+            testProperty "New SingleOrDefault is deterministic" <| quickCompare Enumerable.SingleOrDefault Enumerable.SingleOrDefault id
+            testProperty "New Select is deterministic" <| quickCompare Enumerable.Select Enumerable.Select id
+            testProperty "Original FirstOrDefault is deterministic" <| quickCompare OldEnumerable.FirstOrDefault OldEnumerable.FirstOrDefault id
+            testProperty "Original LastOrDefault is deterministic" <| quickCompare OldEnumerable.LastOrDefault OldEnumerable.LastOrDefault id
+            testProperty "Original SingleOrDefault is deterministic" <| quickCompare OldEnumerable.SingleOrDefault OldEnumerable.SingleOrDefault id
+            testProperty "Old Select is deterministic" <| quickCompare OldEnumerable.Select OldEnumerable.Select id
+        ] |> testSequenced
+        testList "Comparing .NET Core to .NET Framework" [
+            testProperty "FirstOrDefault has not changed" <| quickCompare Enumerable.FirstOrDefault OldEnumerable.FirstOrDefault id
+            testProperty "LastOrDefault has not changed" <| quickCompare Enumerable.LastOrDefault OldEnumerable.LastOrDefault id
+            testProperty "SingleOrDefault has not changed" <| quickCompare Enumerable.SingleOrDefault OldEnumerable.SingleOrDefault id
+            testProperty "Select has not changed" <| quickCompare Enumerable.Select OldEnumerable.Select id
 
-        testProperty "FirstOrDefault has not changed, in isolation" <| quickCompare Enumerable.FirstOrDefault OldEnumerable.FirstOrDefault id
-        testProperty "LastOrDefault has not changed, in isolation" <| quickCompare Enumerable.LastOrDefault OldEnumerable.LastOrDefault id
-        testProperty "SingleOrDefault has not changed, in isolation" <| quickCompare Enumerable.SingleOrDefault OldEnumerable.SingleOrDefault id
-
-        let orderIt (x:int array) = x.OrderBy(fun x -> x)
-        testProperty "FirstOrDefault has not changed on OrderedEnumerable" <| quickCompare Enumerable.FirstOrDefault OldEnumerable.FirstOrDefault orderIt
-        testProperty "LastOrDefault has not changed on OrderedEnumerable" <| quickCompare Enumerable.LastOrDefault OldEnumerable.LastOrDefault orderIt
-        testProperty "SingleOrDefault has not changed on OrderedEnumerable" <| quickCompare Enumerable.SingleOrDefault OldEnumerable.SingleOrDefault orderIt
+            let orderIt (x:int array) = x.OrderBy(fun x -> x)
+            testProperty "FirstOrDefault has not changed on OrderedEnumerable" <| quickCompare Enumerable.FirstOrDefault OldEnumerable.FirstOrDefault orderIt
+            testProperty "LastOrDefault has not changed on OrderedEnumerable" <| quickCompare Enumerable.LastOrDefault OldEnumerable.LastOrDefault orderIt
+            testProperty "SingleOrDefault has not changed on OrderedEnumerable" <| quickCompare Enumerable.SingleOrDefault OldEnumerable.SingleOrDefault orderIt
+            testProperty "Select has not changed on OrderedEnumerable" <| quickCompare Enumerable.Select OldEnumerable.Select orderIt
+        ] |> testSequenced
     ] |> testSequenced
 
 [<EntryPoint>]
