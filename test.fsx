241ce380b1e84c758f2373a9d0bb06975fa7063e
Isaac Abraham <isaac.abraham@gmail.com>
2020-05-17 17:04:01 +0200

    More cleanage

--- test.fsx
+++ test.fsx
@@ -14,11 +14,7 @@ module Generation =
     let inline genHof() = Arb.Default.Function() |> Arb.toGen
     let getFun (Fun f) = f
 
-type Transformer = IEnumerable<int> -> IEnumerable<int> * int
 type Safe<'T> = Result<'T, exn>
-type Aggregator = IEnumerable<int> -> Safe<int * int>
-type Transform = Transform of name:string * old:Transformer * current:Transformer
-type Aggregate = Aggregate of name:string * old:Aggregator * current:Aggregator
 
 module Functions =
     let trackCalls predicate func data =
@@ -29,53 +25,35 @@ module Functions =
         func(data, higherOrderFunction), count
 
     let safely func arg : _ Safe = try Ok(func arg) with ex -> Error ex
-    let makeAggregator name current old (Fun hof) =
-        Aggregate(
-            name,
-            (fun data -> data |> safely (trackCalls hof current)),
-            (fun data -> data |> safely (trackCalls hof old)))
+    type PredicateFun = Function<int, bool>
+    type IdentityFun = Function<int, int>
+    let makeAggregator hof current old =
+        (fun data -> data |> safely (trackCalls hof current)),
+        (fun data -> data |> safely (trackCalls hof old))
+    let makeMapper hof current old =
+        (fun data -> trackCalls hof current data),
+        (fun data -> trackCalls hof old data)
+    type Aggregator =
+        | FirstOrDefault of PredicateFun
+        | LastOrDefault of PredicateFun
+        | SingleOrDefault of PredicateFun
+        | Count of PredicateFun
+        member this.Gen() =
+            match this with
+            | FirstOrDefault (Fun f) -> makeAggregator f Enumerable.FirstOrDefault OldEnumerable.FirstOrDefault
+            | LastOrDefault (Fun f) -> makeAggregator f Enumerable.LastOrDefault OldEnumerable.LastOrDefault
+            | SingleOrDefault (Fun f) -> makeAggregator f Enumerable.SingleOrDefault OldEnumerable.SingleOrDefault
+            | Count (Fun f) -> makeAggregator f Enumerable.Count OldEnumerable.Count
 
-    let makeCollector name current old (Fun hof) =
-        Transform(
-            name,
-            (fun data -> trackCalls hof current data),
-            (fun data -> trackCalls hof old data)
-        )
-
-    let firstOrDefault = makeAggregator "FirstOrDefault" Enumerable.FirstOrDefault OldEnumerable.FirstOrDefault
-    let lastOrDefault = makeAggregator "LastOrDefault" Enumerable.LastOrDefault OldEnumerable.LastOrDefault
-    let singleOrDefault = makeAggregator "SingleOrDefault" Enumerable.SingleOrDefault OldEnumerable.SingleOrDefault
-    let count = makeAggregator "Count" Enumerable.Count OldEnumerable.Count
-
-    let makeSelect = makeCollector "Select" Enumerable.Select OldEnumerable.Select
-    let makeWhere = makeCollector "Where" Enumerable.Where OldEnumerable.Where
-    let makeOrderBy = makeCollector "OrderBy" (fun (a, b:int -> bool) -> Enumerable.OrderBy(a, fun a -> a) :> _) (fun (a, _) -> OldEnumerable.OrderBy(a, fun a -> a) :> _)
-
-let generateAggregator =
-    let aggregator =
-        [ Functions.firstOrDefault
-          Functions.lastOrDefault
-          Functions.singleOrDefault
-          Functions.count ]
-        |> Gen.elements
-
-    gen {
-        let! hof = genHof()
-        let! aggregator = aggregator
-        return aggregator hof
-    }
-
-let generateCollector =
-    [ genHof() |> Gen.map Functions.makeSelect
-      genHof() |> Gen.map Functions.makeOrderBy
-      genHof() |> Gen.map Functions.makeWhere ]
-    |> Gen.oneof
-
-type FunctionsGen =
-    static member Aggregator() =
-        { new Arbitrary<Aggregate>() with override _.Generator = generateAggregator }
-    static member Collector() =
-        { new Arbitrary<Transform>() with override _.Generator = generateCollector }
+    type Mapper =
+        | Select of IdentityFun
+        | Where of PredicateFun
+        | OrderBy of IdentityFun
+        member this.Gen() =
+            match this with
+            | Select (Fun f) -> makeMapper f Enumerable.Select OldEnumerable.Select
+            | Where (Fun f) -> makeMapper f Enumerable.Where OldEnumerable.Where
+            | OrderBy (Fun f) -> makeMapper f (fun (data,b) -> Enumerable.OrderBy(data, (fun value -> b value)) :> _) (fun (data,b) -> OldEnumerable.OrderBy(data, (fun value -> b value)) :> _)
 
 let executePipeline collectors (data:_ seq) =
     collectors
@@ -89,24 +67,26 @@ let executeAggregator aggregator (collectorResult, collectorCount) =
     | Ok (aggregatorResult, aggregationCount) -> Ok(aggregatorResult, (aggregationCount + collectorCount))
     | Error x -> Error x
 
-let collectOnly collectors (data:_ array) =
+let collectOnly (mappers:Functions.Mapper list) (data:_ array) =
     let newPipeline, oldPipeline =
-        collectors
-        |> List.map(fun (Transform(_,b,c)) -> b,c)
+        mappers
+        |> List.map(fun mapper -> mapper.Gen())
         |> List.unzip
     let newResult = data |> executePipeline newPipeline |> (fun (a,b) -> Seq.toArray a, b)
     let oldResult = data |> executePipeline oldPipeline |> (fun (a,b) -> Seq.toArray a, b)
 
     newResult = oldResult
 
-let collectAndAggregate collectors (Aggregate (_, newAgg, oldAgg)) (NonEmptyArray data) =
+let collectAndAggregate (input:{| Data : _ NonEmptyArray; Mappers : Functions.Mapper list; Aggregator:Functions.Aggregator |}) =
     let newPipeline, oldPipeline =
-        collectors
-        |> List.map(fun (Transform(_,b,c)) -> b,c)
+        input.Mappers
+        |> List.map (fun mapper -> mapper.Gen())
         |> List.unzip
-    let newResult = data |> executePipeline newPipeline |> executeAggregator newAgg |> Result.mapError ignore
-    let oldResult = data |> executePipeline oldPipeline |> executeAggregator oldAgg |> Result.mapError ignore
+    let newAgg, oldAgg = input.Aggregator.Gen()
+
+    let newResult = input.Data.Get |> executePipeline newPipeline |> executeAggregator newAgg |> Result.mapError ignore
+    let oldResult = input.Data.Get |> executePipeline oldPipeline |> executeAggregator oldAgg |> Result.mapError ignore
 
     newResult = oldResult
 
-Check.One( { Config.Default with Replay = Some (Random.newSeed()); Arbitrary = [ typeof<FunctionsGen> ] }, collectAndAggregate)
+Check.One({ Config.Default with Replay = Some (Random.newSeed()) }, collectAndAggregate)
